cd71a5d86d8f08fb36313f931172dbd364422867
innovember <berikakhmed@gmail.com>
2021-05-27 00:55:03 +0600

    add models for chat

---
+++ internal/models/chat.go
@@ -0,0 +1,53 @@
+package models
+
+import (
+	"database/sql"
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
+
+type Room struct {
+	ID int64 `json:"id"`
+}
+
+type Message struct {
+	ID            int64  `json:"id"`
+	RoomID        int64  `json:"roomID"`
+	Content       string `json:"content"`
+	MessageDate   int64  `json:"messageDate"`
+	User          *User  `json:"user"`
+	IsYourMessage bool   `json:"isYourMessage"`
+}
+
+type Hub struct {
+	// Registered clients.
+	Clients map[*Client]bool
+
+	// Inbound messages from the clients.
+	Broadcast chan []*Message
+
+	// Register requests from the clients.
+	Register chan *Client
+
+	// Unregister requests from clients.
+	Unregister chan *Client
+
+	MU sync.Mutex
+}
+
+type Client struct {
+	UserID int64
+
+	Hub *Hub
+
+	// The websocket connection.
+	Conn *websocket.Conn
+
+	// Buffered channel of outbound messages.
+	Send chan []*Message
+
+	DB *sql.DB
+
+	MU sync.Mutex
+}
